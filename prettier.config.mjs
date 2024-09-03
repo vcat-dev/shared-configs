6adc0a9878f8cb2ec79386631f3a6c93d20a4888
YG Park <ygpark80@gmail.com>
2024-09-03 20:53:26 +0900

    prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,21 @@
+module.exports = {
+    printWidth: 180,
+    singleQuote: false,
+    jsxSingleQuote: false,
+    semi: false,
+    tabWidth: 4,
+    useTabs: true,
+    bracketSpacing: true,
+    arrowParens: "always",
+    trailingComma: "none",
+    overrides: [
+        {
+            files: ["*.yml", "*.json", "*.js", "*.md"],
+            options: {
+                tabWidth: 2,
+                useTabs: false,
+                semi: true
+            }
+        }
+    ]
+}
