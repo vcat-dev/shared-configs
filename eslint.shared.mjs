8a9271adfda643364385691386c1e03b0f6ff9d5
YG Park <ygpark80@gmail.com>
2024-09-19 16:20:09 +0900

    Oops

--- eslint.shared.mjs
+++ eslint.shared.mjs
@@ -2,7 +2,7 @@ import configs from "./eslint.config.mjs"
 
 export default [
 	{
-		ignores: ["**/dist/*", "*.config.js", "*.config.ts", "*.config.mjs", "*.setup.ts", "src/drizzle/**/*.ts", "src/overrides/**/*.ts", "**/coverage/*"]
+		ignores: ["**/dist/*", "**/*.config.js", "**/*.config.ts", "**/*.config.mjs", "**/*.setup.ts", "src/drizzle/**/*.ts", "src/overrides/**/*.ts", "**/coverage/*"]
 	},
 	...configs
 ]
